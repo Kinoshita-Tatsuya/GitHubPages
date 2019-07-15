fbd5a330e52d094f8056ff4ca09ebefca5bd01f0
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-07-15 14:03:03 +0900

    jsファイルのリファクタリング

--- docs/TagClick.js
+++ docs/TagClick.js
@@ -1,60 +1,84 @@
-var ProfileTagId = document.getElementById("ProfileTag");
-var TagList = document.getElementsByClassName("Tag");
-var TagTextList = document.getElementsByClassName("Tag_Text");
-
-function MoveAllTag()
+const MoveAllTag = () =>
 {
-    var id = document.getElementsByClassName("Catchphrase");
-    for(let i = 0;i < TagList.length; ++i)
-    {
-        TagList[i].classList.add("Isclick");
-    }
+    let TagList = document.getElementsByClassName("Tag");
+    let TagTextList = document.getElementsByClassName("Tag_Text");
+    let catchphrase = document.getElementsByClassName("Catchphrase");
     
-    for(let i = 0;i < TagTextList.length; ++i)
-    {
-        TagTextList[i].classList.add("Isclick");
-    }
+    for(let i = 0;i < TagList.length; ++i)　TagList[i].classList.add("isclick");
+    
+    for(let i = 0;i < TagTextList.length; ++i)　TagTextList[i].classList.add("isclick");
 
-    id.item(0).classList.add("IsClick");
+    catchphrase.item(0).classList.add("isClick");
 }
 
-function IsClickGameTag()
+const DestroyByTags = () =>
 {
-    MoveAllTag();
-    setTimeout(function()
-    {
-        var children = document.getElementById("GameTag").children;
+    let TagList = document.getElementsByClassName("Tag");
+    let TagTextList = document.getElementsByClassName("Tag_Text");
 
-        children[0].classList.remove("Tag");
-        children[0].classList.remove("IsClick");
-        children[0].classList.add("Game_Description");
-        children[0].classList.add("SlideIn");
-    },3000)
+    for(let i = TagList.length - 1;i >= 0; --i) TagList[i].remove(TagList[i]);
+
+    for(let i = TagTextList.length - 1;i >= 0; --i) TagTextList[i].remove(TagTextList[i]);
 }
 
-function IsClickProfileTag()
+const IsClickGameTag = () =>
 {
     MoveAllTag();
-    setTimeout(function()
+    setTimeout( () =>
+    {
+        DestroyByTags();
+        ExplainGames();   
+    },1000)
+}
+
+const IsClickProfileTag = () =>
+{
+    MoveAllTag();
+    setTimeout( () =>
     {
         
     },3000)
 }
 
-function IsClickTopTag()
+const IsClickTopTag = () =>
 {
     MoveAllTag();
-    setTimeout(function()
+    setTimeout( () =>
     {
         
     },3000)
 }
 
-function IsClickThisPageTag()
+const IsClickThisPageTag= () =>
 {
     MoveAllTag();
-    setTimeout(function()
+    setTimeout( () =>
     {
         
     },3000)
 }
+
+const ExplainGames = () =>
+{
+    let gameTag = document.getElementById("GameTag");
+    const SlideInClassName = "SlideIn";
+
+    //ゲームの説明のタグを表示する
+    let Game_DescDiv = document.createElement("div");
+    Game_DescDiv.classList.add("Game_Description");
+    Game_DescDiv.classList.add(SlideInClassName);
+    gameTag.appendChild(Game_DescDiv);
+    
+    //スターライトラインの説明画像クラスの追加
+    let SLLDiv = document.createElement("div");
+    SLLDiv.classList.add("StarlightLine_Description");
+    SLLDiv.classList.add(SlideInClassName);
+    gameTag.appendChild(SLLDiv);
+
+    //スターライトラインの説明テキスト文クラスの追加
+    let SLLDescText = document.createElement("div");
+    SLLDescText.innerText = "制作期間:  3ヶ月(2018/10~2019/1)\nプラットフォーム:  PC\nジャンル:  星わたりアクションアドベンチャー\n\n流れてくる星を限られたラインの中で避けたり拾ったりして\n高スコアを目指せ！";
+    SLLDescText.classList.add("SLLText");
+    SLLDescText.classList.add(SlideInClassName);
+    gameTag.appendChild(SLLDescText);
+} 
