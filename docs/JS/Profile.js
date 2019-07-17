6e12c94438c3cf11d87d81a59b68c9897a30a910
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-07-17 23:19:10 +0900

    コメントの追加とクラスの名前変更

--- docs/JS/Profile.js
+++ docs/JS/Profile.js
@@ -11,39 +11,42 @@ const CreateProfileDivs = () =>
     let profileTag = document.getElementById("ProfileTag");
     const FadeInstr = "FadeIn";
 
+    //プロフィールの説明文字の表示
     let profile = document.createElement("div");
-    profile.classList.add("TitleProfile");
+    profile.classList.add("Title_Profile");
     profile.classList.add(FadeInstr);
     profileTag.appendChild(profile);
 
+    //自分の名前の表示
     let name = document.createElement("div");
     name.innerText　= "Name"
     name.classList.add("MyName");
     name.classList.add(FadeInstr);
     profileTag.appendChild(name);
 
+    //誕生日の表示
     let birth = document.createElement("div");
     birth.innerText　= "Birth"
     birth.classList.add("Birth");
     birth.classList.add(FadeInstr);
     profileTag.appendChild(birth);
 
+    //趣味の表示
     let hobby = document.createElement("div");
     hobby.innerText　= "Hobby"
     hobby.classList.add("Hobby");
     hobby.classList.add(FadeInstr);
     profileTag.appendChild(hobby);
 
+    //トップページに戻れるdivの追加
     let JumpTopPage = document.createElement("div");
     JumpTopPage.innerText　= "TopPageへ"
     JumpTopPage.classList.add("JumpTopPage");
     JumpTopPage.classList.add(FadeInstr);
-
     JumpTopPage.onclick = () =>
     {
         location.reload();
     }
 
     profileTag.appendChild(JumpTopPage);
-
 }
