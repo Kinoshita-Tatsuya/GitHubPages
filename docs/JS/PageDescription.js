32ca673365be835968cd28b50108a36c4abf5802
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-07-22 02:06:38 +0900

    頑張ったところページを一つのhtmlファイルで行うように変更

---
+++ docs/JS/PageDescription.js
@@ -0,0 +1,44 @@
+const ShowPageDescription = () =>
+{
+    setTimeout(() =>
+    {
+        CreatePageDescriptionDivs();
+    },1000);
+}
+
+const CreatePageDescriptionDivs = () =>
+{
+    let thisPageTag = document.getElementById("ThisPageTag");
+
+    let title = document.createElement("h2");
+    title.innerText = "頑張ったところ";
+    thisPageTag.appendChild(title);
+
+    let innerText = 
+    [
+        "スクロールをできるページとできないページを分けたところ",
+        "ページごとにJSファイルを作成し、そのページで何をやっているのかをわかりやすくしたところ",
+        "そのページごとのアニメーションを作成し、動きをつけたところ",
+        "ゲーム説明の画像を押すとgitHubのリポジトリに行けるようにしたところ",
+        "いかにおしゃれにできるかを考えて位置調整や色の調整をしたところ"
+    ];
+
+    innerText.forEach((item ,index) =>
+    {
+        let elm = document.createElement("li");
+        elm.innerText = innerText[index];
+        thisPageTag.appendChild(elm);
+    });
+
+    let topPage = document.createElement("div");
+    topPage.classList.add("From_ThisPage_To_TopPage");
+    topPage.classList.add("SlideFadeIn");
+    topPage.innerText = "TopPageへ";
+
+    topPage.onclick = () =>
+    {
+        location.reload();
+    }
+
+    thisPageTag.appendChild(topPage);
+}
